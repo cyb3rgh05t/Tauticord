a3a6191e1830fb7f7c2df7d3f4b92418a9463b6e
cyb3rgh05t <flammang.yves@gmail.com>
2022-08-31 18:45:10 +0200

    Update Tauticord.py

--- Tauticord.py
+++ Tauticord.py
@@ -9,7 +9,7 @@ import modules.tautulli_connector as tautulli
 from modules import config_parser
 from modules.logs import *
 
-config = config_parser.Config(app_name="Tauticord", config_path="config.yaml")
+config = config_parser.Config(app_name="Tauticord", config_path="config/config.yaml")
 
 analytics = GoogleAnalytics(analytics_id='UA-174268200-2',
                             anonymous_ip=True,
