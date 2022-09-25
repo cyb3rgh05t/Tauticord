037c19e100b9ba5a2bb22fe8a99a27e2ff62066b
cyb3rgh05t <flammang.yves@gmail.com>
2022-09-25 10:30:31 +0200

    fix config path

--- Tauticord.py
+++ Tauticord.py
@@ -10,7 +10,7 @@ import modules.tautulli_connector as tautulli
 from modules import config_parser
 from modules.logs import *
 
-config = config_parser.Config(app_name="Tauticord", config_path=f"{Path(Path(__file__).parent / 'config.yaml')}")
+config = config_parser.Config(app_name="Tauticord", config_path="config/config.yaml")
 
 analytics = GoogleAnalytics(analytics_id='UA-174268200-2',
                             anonymous_ip=True,
